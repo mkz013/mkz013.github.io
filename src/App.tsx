abfca2c521016a6719b48872878e8da21aed64eb
Anthony <antonie.ochisor@student.howest.be>
2026-07-13 01:41:01 +0200

    feat: add about section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Hero } from './sections/Hero'
+import {About} from "./sections/About.tsx";
 
 function App() {
   return (
       <div className="min-h-screen bg-[var(--bg-main)] text-[var(--text-main)]">
         <main className="mx-auto flex min-h-screen w-full max-w-6xl flex-col px-6 py-8 md:px-10 md:py-10">
           <Hero />
+          <About />
         </main>
       </div>
   )
